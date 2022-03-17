024465c22ba9186fe307076039f7ce9f4521a446
Astitva Veer Garg <av4764@srmist.edu.in>
2022-03-18 03:04:00 +0530

    Improvement 2.0

--- Api.py
+++ Api.py
@@ -11,6 +11,8 @@ def main():
 
 @app.route('/symptoms=<string:symptomsgg>')
 def Predicted(symptomsgg):
+    global l1
+    global l2
     l1=['back_pain','constipation','abdominal_pain','diarrhoea','mild_fever','yellow_urine',
     'yellowing_of_eyes','acute_liver_failure','fluid_overload','swelling_of_stomach',
     'swelled_lymph_nodes','malaise','blurred_and_distorted_vision','phlegm','throat_irritation',
@@ -46,7 +48,7 @@ def Predicted(symptomsgg):
     for x in range(0,len(l1)):
         l2.append(0)
 
-    dataset=pd.read_csv(r"./Dataset/Training.csv")
+    dataset=pd.read_csv(r"./dataset/Training.csv")
 
     dataset.replace({'prognosis':{'Fungal infection':0,'Allergy':1,'GERD':2,'Chronic cholestasis':3,'Drug Reaction':4,
     'Peptic ulcer diseae':5,'AIDS':6,'Diabetes ':7,'Gastroenteritis':8,'Bronchial Asthma':9,'Hypertension ':10,
@@ -64,7 +66,7 @@ def Predicted(symptomsgg):
     y = dataset[["prognosis"]]
     np.ravel(y)
 
-    tr=pd.read_csv(r"./Dataset/Testing.csv")
+    tr=pd.read_csv(r"./dataset/Testing.csv")
     tr.replace({'prognosis':{'Fungal infection':0,'Allergy':1,'GERD':2,'Chronic cholestasis':3,'Drug Reaction':4,
     'Peptic ulcer diseae':5,'AIDS':6,'Diabetes ':7,'Gastroenteritis':8,'Bronchial Asthma':9,'Hypertension ':10,
     'Migraine':11,'Cervical spondylosis':12,'Paralysis (brain hemorrhage)':13,'Jaundice':14,'Malaria':15,'Chicken pox':16,'Dengue':17,'Typhoid':18,'hepatitis A':19,'Hepatitis B':20,'Hepatitis C':21,'Hepatitis D':22,'Hepatitis E':23,'Alcoholic hepatitis':24,'Tuberculosis':25,'Common Cold':26,'Pneumonia':27,'Dimorphic hemmorhoids(piles)':28,'Heart attack':29,'Varicose veins':30, 'Hypothyroidism':31,'Hyperthyroidism':32,'Hypoglycemia':33,'Osteoarthristis':34,'Arthritis':35,'(vertigo) Paroymsal  Positional Vertigo':36,'Acne':37,'Urinary tract infection':38,'Psoriasis':39,'Impetigo':40}},inplace=True)
@@ -191,5 +193,29 @@ def Predicted(symptomsgg):
     return jsonify(result)
 
 
+@app.route('/symptom')
+def Symptom():
+    l1=['back_pain','constipation','abdominal_pain','diarrhoea','mild_fever','yellow_urine',
+    'yellowing_of_eyes','acute_liver_failure','fluid_overload','swelling_of_stomach',
+    'swelled_lymph_nodes','malaise','blurred_and_distorted_vision','phlegm','throat_irritation',
+    'redness_of_eyes','sinus_pressure','runny_nose','congestion','chest_pain','weakness_in_limbs',
+    'fast_heart_rate','pain_during_bowel_movements','pain_in_anal_region','bloody_stool',
+    'irritation_in_anus','neck_pain','dizziness','cramps','bruising','obesity','swollen_legs',
+    'swollen_blood_vessels','puffy_face_and_eyes','enlarged_thyroid','brittle_nails',
+    'swollen_extremeties','excessive_hunger','extra_marital_contacts','drying_and_tingling_lips',
+    'slurred_speech','knee_pain','hip_joint_pain','muscle_weakness','stiff_neck','swelling_joints',
+    'movement_stiffness','spinning_movements','loss_of_balance','unsteadiness',
+    'weakness_of_one_body_side','loss_of_smell','bladder_discomfort','foul_smell_of urine',
+    'continuous_feel_of_urine','passage_of_gases','internal_itching','toxic_look_(typhos)',
+    'depression','irritability','muscle_pain','altered_sensorium','red_spots_over_body','belly_pain',
+    'abnormal_menstruation','dischromic _patches','watering_from_eyes','increased_appetite','polyuria','family_history','mucoid_sputum',
+    'rusty_sputum','lack_of_concentration','visual_disturbances','receiving_blood_transfusion',
+    'receiving_unsterile_injections','coma','stomach_bleeding','distention_of_abdomen',
+    'history_of_alcohol_consumption','fluid_overload','blood_in_sputum','prominent_veins_on_calf',
+    'palpitations','painful_walking','pus_filled_pimples','blackheads','scurring','skin_peeling',
+    'silver_like_dusting','small_dents_in_nails','inflammatory_nails','blister','red_sore_around_nose',
+    'yellow_crust_ooze']
+    return jsonify(l1)
+
 if __name__ == '__main__':
-    app.run(threaded=True,debug=True)
+    app.run(threaded=False,debug=True)
